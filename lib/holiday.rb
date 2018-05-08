6c3e29b32b214cb7189327401a12099bfa5e1aed
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-08 15:45:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,6 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
-  return holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
